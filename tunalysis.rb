358268e27754a0dbcb5202853f681da9c98f9f2b
Florent Delannoy <florent.delannoy@gmail.com>
2010-11-21 02:34:34 +0100

    Added shebang

--- tunalysis.rb
+++ tunalysis.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 # ---------------------------------------------------------------------
 # Tunalysis is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
